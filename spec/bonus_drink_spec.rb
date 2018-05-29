d7c88f10810dbceed7f817334f8a7301135cee2c
hokupod <fireface800rme@gmail.com>
2018-05-30 00:06:19 +0900

    Add no gift case.

--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  describe 'no gift' do
+    context 'case of 0' do
+      specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+    end
+    (1..2).each do |num|
+      context "case of #{num}" do
+        specify { expect(BonusDrink.total_count_for(num)).to eq num }
+      end
+    end
+  end
 end
