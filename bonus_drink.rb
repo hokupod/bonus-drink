53a55f9f9737cb3bb945792e52213743072161bb
hokupod <fireface800rme@gmail.com>
2018-05-30 00:44:43 +0900

    Change bonus calculation.

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -2,12 +2,13 @@ class BonusDrink
   BONUS_THRESHOLD = 3
   class << self
     def total_count_for(amount)
+      return amount if amount < BONUS_THRESHOLD
       amount + bonus(amount)
     end
 
     private
     def bonus(amount)
-      (amount / BONUS_THRESHOLD).to_i
+      (amount % BONUS_THRESHOLD + amount / BONUS_THRESHOLD).to_i
     end
   end
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -2,13 +2,21 @@ require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
   describe 'no bonus' do
-    context 'case of 0' do
-      specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
-    end
-    (1..2).each do |num|
-      context "case of #{num}" do
+    (0..2).each do |num|
+      context "amount is #{num}" do
         specify { expect(BonusDrink.total_count_for(num)).to eq num }
       end
+    end
+  end
+  describe 'get bonus' do
+    context 'amount is 3' do
+      specify { expect(BonusDrink.total_count_for(3)).to eq 4 }
+    end
+    context 'amount is 11' do
+      specify { expect(BonusDrink.total_count_for(11)).to eq 16 }
+    end
+    context 'amount is 100' do
+      specify { expect(BonusDrink.total_count_for(100)).to eq 134 }
     end
   end
 end
