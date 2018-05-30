6eb26f4c684305075f93c5f312112ed5177f7bfc
hokupod <fireface800rme@gmail.com>
2018-05-30 23:18:09 +0900

    refactoring.

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,16 +1,16 @@
 class BonusDrink
   BONUS_THRESHOLD = 3
+  BONUS_DRINK = 1
   class << self
     def total_count_for(amount)
-      return amount if amount < BONUS_THRESHOLD
       amount + bonus(amount)
     end
 
     private
     def bonus(base_amount, acc = 0)
       return acc if base_amount < BONUS_THRESHOLD
-      acc += 1
-      bonus(base_amount - BONUS_THRESHOLD + 1, acc)
+      acc += BONUS_DRINK
+      bonus(base_amount - BONUS_THRESHOLD + BONUS_DRINK, acc)
     end
   end
 end
